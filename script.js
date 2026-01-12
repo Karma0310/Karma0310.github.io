2b1a3b91fb05b940761fc30bdfee54afad64ab35
Karma0310 <KarmaFIt@proton.me>
2026-01-12 17:49:01 -0300

    a

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
-// LIGHTBOX
-const lightbox = document.getElementById("lightbox");
-const lightboxImg = document.getElementById("lightbox-img");
+document.addEventListener("DOMContentLoaded", () => {
 
-document.querySelectorAll("[data-lightbox]").forEach(img => {
-  img.addEventListSner("click", () => {
-    lightboxImg.src = img.src;
-    lightbox.style.display = "flex";
+  // LIGHTBOX ELEMENTS
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+
+  // PROJECT IMAGES
+  document.querySelectorAll("[data-lightbox]").forEach(img => {
+    img.addEventListener("click", () => {
+      lightboxImg.src = img.src;
+      lightbox.style.display = "flex";
+    });
   });
-});
 
-document.querySelectorAll(".document-card").forEach(card => {
-  card.addEventListener("click", () => {
-    const src = "images/" + card.dataset.doc;
-    lightboxImg.src = src;
-    lightbox.style.display = "flex";
+  // DOCUMENT CARDS
+  document.querySelectorAll(".document-card").forEach(card => {
+    card.addEventListener("click", () => {
+      const src = "images/" + card.getAttribute("data-doc");
+      lightboxImg.src = src;
+      lightbox.style.display = "flex";
+    }); 
+     document.addEventListener("keydown", e => {
+       if (e.key === "Escape") {
+      lightbox.style.display = "none";
+       }
+      });
   });
-});
 
-lightbox.addEventListener("click", () => {
-  lightbox.style.display = "none";
-});
+  // CLOSE LIGHTBOX
+  lightbox.addEventListener("click", () => {
+    lightbox.style.display = "none";
+    lightboxImg.src = "";
+  });
 
-// BACK TO TOP
-const backToTop = document.getElementById("backToTop");
+  // BACK TO TOP
+  const backToTop = document.getElementById("backToTop");
 
-window.addEventListener("scroll", () => {
-  backToTop.style.display =
-    document.documentElement.scrollTop > 400 ? "block" : "none";
-});
+  window.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop > 400) {
+      backToTop.style.display = "block";
+    } else {
+      backToTop.style.display = "none";
+    }
+  });
 
-backToTop.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
+  backToTop.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
+
+
 });
